a60fc0a0dfd29417c2453551303e7f9a77b9d07c
Vulcanric <jneric49@gmail.com>
2023-07-22 04:06:40 +0100

    updated

--- main.h
+++ main.h
@@ -14,4 +14,8 @@
 void display_prompt(void);
 void handle_cmd(char **env, char *line_ptr, char *delim);
 int _strcmp(char *s1, char *s2);
+char *path(char *cmd);
+char *_strcat(char *dest, char *src);
+void _perror(char *cmd);
+void print_num(int n);
 #endif
